ccfb2680ffbca10eb8ef77b0866df79aa3945cf1
egedolmaci <dolmaciege@gmail.com>
2025-09-24 14:19:31 +0300

    feat(sliding_window_log): start implementing test for sliding

--- internal/strategies/sliding_window_log_test.go
+++ internal/strategies/sliding_window_log_test.go
@@ -15,17 +15,4 @@ func TestSlidingWindowLogStrategy(t *testing.T) {
 			t.Errorf("Request should be allowed got %t, expected %t", allowed, true)
 		}
 	})
-
-	t.Run("# of requests above limit", func(t *testing.T) {
-		strategy := SlidingWindowLogStrategy{2, time.Minute}
-
-		strategy.IsRequestAllowed("ege")
-		strategy.IsRequestAllowed("ege")
-		allowed, _ := strategy.IsRequestAllowed("ege")
-
-		if allowed {
-			t.Errorf("Limit is 2, this is 3rd request should not be allowed, got %t, expected %t", allowed, false)
-		}
-	})
-	
 }
