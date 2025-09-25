7c825b6e908a0f312002512ea0abfadf69dba5ac
egedolmaci <dolmaciege@gmail.com>
2025-09-25 14:58:23 +0300

    feat(sliding_window_log): implement generic storage

--- internal/strategies/sliding_window_log.go
+++ internal/strategies/sliding_window_log.go
@@ -5,14 +5,40 @@ import "time"
 type SlidingWindowLogStrategy struct {
 	limit int
 	windowSize time.Duration
+	storage map[string]int
 }
 
 func NewSlidingWindowLogStrategy(limit int, windowSize time.Duration) *SlidingWindowLogStrategy {{
-	return &SlidingWindowLogStrategy{limit, windowSize}
+	return &SlidingWindowLogStrategy{
+		limit,
+		windowSize,
+		map[string]int{},
+	}
 }}
 
 func (s *SlidingWindowLogStrategy) IsRequestAllowed(identifier string) (bool, int) {
-	return true, 0
+	return s.checkStorage(identifier)
+}
+
+func (s *SlidingWindowLogStrategy) checkStorage(identifier string) (bool, int) {
+	for id, count := range s.storage {
+		if id == identifier {
+			if count < s.limit {
+				s.addToStorage(identifier)
+				return true, s.limit - count
+			} else {
+				return false, 0
+			}
+		} 
+	}
+
+	s.addToStorage(identifier)
+	return true, s.limit 
+}
+
+func (s *SlidingWindowLogStrategy) addToStorage(identifier string) {
+	s.storage[identifier]++
 }
 
 func (s *SlidingWindowLogStrategy) Stop() {}
+
